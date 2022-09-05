92f07f4c1e3e05829e85dc5a2f7260150effb2d1
Tran Hoang Anh <anh.embedded@gmail.com>
2022-09-05 12:45:02 +0700

    Done

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
  * @brief
  * @version 0.1
  * @date 2022-08-29
- *
+ * @note Chỉ phân loại được các nhóm, chưa thể tính thông số "nghiêm trọng" của từng nhóm.
  * @copyright Copyright (c) 2022
  *
  */
@@ -17,6 +17,7 @@
 
 int main( ) {
 
+//! @note Chỉ phân loại được các nhóm, chưa thể tính thông số "nghiêm trọng" của từng nhóm.
     dataCovid_T dataCovid;
     dataCovid = dataCovidInit(data_numberPeople , data_pair ,
                               data_numberOfContactedPair , data_ArrayF0 , data_numberF0);
